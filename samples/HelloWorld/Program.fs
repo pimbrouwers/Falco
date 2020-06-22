8ae91fe0d68175cd85232e0717f445cbf9d35408
Pim Brouwers <pbrouwers@nhlpa.com>
2020-06-22 16:02:29 -0400

    explicit middleware file for aspcore IApplicationBuilder ext methods, converted hello world to non-comp expr example

--- samples/HelloWorld/Program.fs
+++ samples/HelloWorld/Program.fs
@@ -1,8 +1,35 @@
 module HelloWorldApp 
 
 open Falco
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
 
-webApp {        
-    get "/"  (textOut "hello")
-    notFound (setStatusCode 404 >=> textOut "Not found")
-}
+let routes = 
+    [
+        get "/"  (textOut "hello world")
+    ]
+
+let configureServices (services : IServiceCollection) =
+    services.AddRouting() 
+    |> ignore
+
+let configureApp (app : IApplicationBuilder) = 
+    app.UseRouting()
+       .UseHttpEndPoints(routes)
+       .UseNotFoundHandler(setStatusCode 404 >=> textOut "Not found")
+       |> ignore 
+
+[<EntryPoint>]
+let main _ =
+    try
+        WebHostBuilder()
+            .UseKestrel()
+            .ConfigureServices(configureServices)
+            .Configure(configureApp)
+            .Build()
+            .Run()
+
+        0
+    with 
+        | _ -> -1
