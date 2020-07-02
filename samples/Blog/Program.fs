93acfa8c82d4fbb9f8df3e30635f07f72b8e74b2
pimbrouwers <pimnation@gmail.com>
2020-07-02 09:57:36 -0400

    samples: primitives introduce in Env

--- samples/Blog/Program.fs
+++ samples/Blog/Program.fs
@@ -1,27 +1,34 @@
 module Blog.Program
 
-open Blog.Server
 
 module Env =
     open System
     open System.IO
     open Falco.StringUtils
+    open Blog.Server
 
     let root = Directory.GetCurrentDirectory()
 
-    let postsDirectory = Path.Combine(root, "Posts")
+    let postsDirectory = 
+        Path.Combine(root, "Posts") 
+        |> PostsDirectory
     
-    let tryGetEnv = Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+    let tryGetEnv (name : string) = 
+        match Environment.GetEnvironmentVariable name with 
+        | null 
+        | ""    -> None 
+        | value -> Some value
 
-    let developerMode = match tryGetEnv "ASPNETCORE_ENVIRONMENT" with None -> true | Some env -> strEquals env "development"
+    let developerMode = 
+        match tryGetEnv "ASPNETCORE_ENVIRONMENT" with 
+        | None     -> true 
+        | Some env -> strEquals env "development" 
+        |> DeveloperMode
 
 [<EntryPoint>]
 let main _ =
-    try
-        let developerMode = DeveloperMode Env.developerMode
-        let postsDirectory = PostsDirectory Env.postsDirectory
-
-        Server.buildServer developerMode postsDirectory
+    try        
+        Server.buildServer Env.developerMode Env.postsDirectory
         |> Server.startServer
         0
     with
--- samples/HelloWorld/Program.fs
+++ samples/HelloWorld/Program.fs
@@ -1,21 +1,27 @@
 module HelloWorld.Program
 
-open HelloWorld.Server
 
 module Env =    
     open System    
     open Falco.StringUtils
+    open HelloWorld.Server
+    
+    let tryGetEnv (name : string) = 
+        match Environment.GetEnvironmentVariable name with 
+        | null 
+        | ""    -> None 
+        | value -> Some value
 
-    let tryGetEnv = Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
-
-    let developerMode = match tryGetEnv "ASPNETCORE_ENVIRONMENT" with None -> true | Some env -> strEquals env "development"
+    let developerMode = 
+        match tryGetEnv "ASPNETCORE_ENVIRONMENT" with 
+        | None     -> true 
+        | Some env -> strEquals env "development"
+        |> DeveloperMode
 
 [<EntryPoint>]
 let main _ =        
     try
-        let developerMode = DeveloperMode Env.developerMode
-
-        Server.buildServer developerMode
+        Server.buildServer Env.developerMode
         |> Server.startServer
         0
     with
