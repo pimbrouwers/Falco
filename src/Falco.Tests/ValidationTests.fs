5562ebbe59d99dc167d32a19510956023b2928c2
Pim Brouwers <pimnation@gmail.com>
2020-11-20 15:05:00 -0500

    pre validation removal

--- src/Falco.Tests/ValidationTests.fs
+++ src/Falco.Tests/ValidationTests.fs
@@ -29,6 +29,7 @@ let ``ValidationError.merges produces Map<string, string list> from two sources 
 [<Fact>]
 let ``ValidationResult.create produces Ok result`` () =    
     ValidationResult.create true () ValidationErrors.empty
+    |> ValidationResult.toResult
     |> Result.bind (fun result -> Ok (result |> should equal ()))
 
 [<Fact>]
@@ -36,45 +37,64 @@ let ``ValidationResult.create produces Error result`` () =
     let errorMessage = "fake error message"
     let error = ValidationErrors.create "fakeField" [ errorMessage ]
     ValidationResult.create false () error
+    |> ValidationResult.toResult
     |> Result.mapError (fun errors -> errors |> should equal error)
 
 [<Fact>]
 let ``Validation of record succeeds`` () =        
-    let expected : FakeValidationRecord = { Name = "John"; Age = 1 }
+    let expected : FakeValidationRecord = { Name = "John"; Age = 1 }    
     let result : ValidationResult<FakeValidationRecord> = 
+        let nameValidator =             
+            Validators.String.greaterThanLen 2 None 
+            <+> Validators.String.lessThanLen 100 None 
+
         fun name age -> { 
             Name = name
             Age = age
         }
-        <!> Validators.String.greaterThanLen 2 "Name" None "John"
-        <*> Validators.Int.greaterThan 0 "Age" None 1
+        <!> nameValidator "Name" expected.Name       
+        <*> Validators.Int.greaterThan 0 None "Age" 1
     
     result 
+    |> ValidationResult.toResult
     |> Result.bind (fun r -> Ok(r |> should equal expected))
 
-[<Fact>]
-let ``Validation of record with option succeeds`` () =        
-    let expected : FakeValidationRecordWithOption = { Name = "John"; Age = None }
-    let result : ValidationResult<FakeValidationRecordWithOption> = 
-        fun name age -> { 
-            Name = name
-            Age = age
-        }
-        <!> Validators.String.greaterThanLen 2 "Name" None "John"
-        <*> Validators.optional (Validators.Int.greaterThan 0) "Age" None None
+//[<Fact>]
+//let ``Validation of record with option succeeds`` () =        
+//    let expected : FakeValidationRecordWithOption = { Name = "John"; Age = None }
+//    let result : ValidationResult<FakeValidationRecordWithOption> = 
+//        let nameValidator = 
+//            Validators.String.greaterThanLen 2 None "Name" expected.Name
+
+//        let ageValidator = 
+//            Validators.optional (Validators.Int.greaterThan 0 None) "Age" expected.Age
+
+//        fun name age -> { 
+//            Name = name
+//            Age = age
+//        }
+//        <!> nameValidator
+//        <*> ageValidator
     
-    result 
-    |> Result.bind (fun r -> Ok(r |> should equal expected))
+//    result 
+//    |> ValidationResult.bind (fun r -> Success(r |> should equal expected))
 
 [<Fact>]
 let ``Validation of record fails`` () =       
-    let result : ValidationResult<FakeValidationRecord> = 
+    let name = "Jo"
+    let age = 3
+    let result : ValidationResult<FakeValidationRecord> =         
+        let nameValidator =             
+            Validators.String.greaterThanLen 2 None
+            <+> Validators.String.lessThanLen 100 None 
+
         fun name age -> { 
             Name = name
             Age = age
         }
-        <!> Validators.String.greaterThanLen 2 "Name" None "Jo"
-        <*> Validators.Int.greaterThan 0 "Age" None 0
+        <!> nameValidator "Name" name
+        <*> Validators.Int.greaterThan 3 None "Age" age
     
     result 
+    |> ValidationResult.toResult
     |> Result.mapError (fun r -> (r.ContainsKey "Name", r.ContainsKey "Age") |> should equal (true, true))
--- src/Falco/Validation.fs
+++ src/Falco/Validation.fs
@@ -4,8 +4,8 @@ open System
 open Falco.StringUtils
 
 type ValidationErrors = Map<string, string list>
-type ValidationResult<'a> = Result<'a, ValidationErrors>
-type Validator<'a> = string -> string option-> 'a -> ValidationResult<'a>
+type ValidationResult<'a> = Success of 'a | Failure of ValidationErrors
+type Validator<'a> = string -> 'a -> ValidationResult<'a>
 
 module ValidationErrors =
     let empty : ValidationErrors = Map.empty<string, string list>
@@ -23,31 +23,54 @@ module ValidationErrors =
             e2
 
 module ValidationResult = 
-    /// Create a ValidationResult<'a> based on condition, yield
-    /// error message if condition evaluates false
-    let create condition value error : ValidationResult<'a> =
-        if condition then Ok value
-        else error |> Error
 
     /// Unpack ValidationResult and feed into validation function
     let apply (resultFn : ValidationResult<'a -> 'b>) (result : ValidationResult<'a>) : ValidationResult<'b> =
         match resultFn, result with
-        | Ok fn, Ok x        -> fn x |> Ok
-        | Error e, Ok _      -> Error e
-        | Ok _, Error e      -> Error e
-        | Error e1, Error e2 -> Error (ValidationErrors.merge e1 e2)  
+        | Success fn, Success x  -> fn x |> Success
+        | Failure e, Success _   -> Failure e
+        | Success _, Failure e   -> Failure e
+        | Failure e1, Failure e2 -> Failure (ValidationErrors.merge e1 e2)  
 
-    /// Unpack ValidationResult, evaluate function if Ok or return if Error
+    /// Unpack ValidationResult and apply inner value to function
+    let bind (fn : 'a -> ValidationResult<'b>) (result : ValidationResult<'a>) : ValidationResult<'b> =
+        match result with 
+        | Success x -> fn x
+        | Failure e -> Failure e
+
+    /// Combine two ValidationResult
+    let combine (a : ValidationResult<'a>) (b : ValidationResult<'a>) =
+        match a, b with
+        | Success a', Success _  -> Success a'
+        | Failure e, Success _   -> Failure e
+        | Success _, Failure e   -> Failure e
+        | Failure e1, Failure e2 -> Failure (ValidationErrors.merge e1 e2)  
+
+    /// Create a ValidationResult<'a> based on condition, yield
+    /// error message if condition evaluates false
+    let create condition value error : ValidationResult<'a> =
+        if condition then Success value
+        else error |> Failure
+
+    /// Unpack ValidationResult, evaluate function if Success or return if Failure
     let map (fn : 'a -> 'b) (result : ValidationResult<'a>) : ValidationResult<'b> =
         match result with 
-        | Ok x    -> fn x |> Ok
-        | Error e -> Error e
+        | Success x -> fn x |> Success
+        | Failure e -> Failure e
 
-/// Custom operators for ValidationResult
-module Operators =
-    let (<*>) = ValidationResult.apply
-    let (<!>) = ValidationResult.map
+    /// Transform ValidationResult<'a> to Result<'a, ValidationErrors>
+    let toResult (result : ValidationResult<'a>) : Result<'a, ValidationErrors> =
+        match result with 
+        | Success r -> Ok r
+        | Failure e -> Error e
 
+module Validator = 
+    let compose (a : Validator<'a>) (b : Validator<'a>) =
+        fun field value -> 
+            ValidationResult.combine
+                (a field value)
+                (b field value)                  
+            
 [<RequireQualifiedAccess>]
 module Validators =
     let private messageOrDefault (fieldName : string) (message : string option) (defaultMessage : unit -> string) =        
@@ -55,78 +78,90 @@ module Validators =
         ValidationErrors.create fieldName [message']
 
     type EqualityValidator<'a when 'a : equality>() =                 
-        member _.equals (equalTo : 'a) : Validator<'a> =
-            fun (fieldName : string) (message : string option) (value : 'a) -> 
-                let defaultMessage () = sprintf "%s must be equal to %A" fieldName equalTo
-                ValidationResult.create (value = equalTo) value (messageOrDefault fieldName message defaultMessage)
+        member _.equals (equalTo : 'a) (message : string option) : Validator<'a> =
+            fun (field : string) (value : 'a) -> 
+                let defaultMessage () = sprintf "%s must be equal to %A" field equalTo
+                ValidationResult.create (value = equalTo) value (messageOrDefault field message defaultMessage)
 
-        member _.notEquals (notEqualTo : 'a) : Validator<'a> =
-            fun (fieldName : string) (message : string option) (value : 'a) -> 
-                let defaultMessage () = sprintf "%s must not equal %A" fieldName notEqualTo
-                ValidationResult.create (value <> notEqualTo) value (messageOrDefault fieldName message defaultMessage)    
+        member _.notEquals (notEqualTo : 'a) (message : string option) : Validator<'a> =
+            fun (field : string) (value : 'a) -> 
+                let defaultMessage () = sprintf "%s must not equal %A" field notEqualTo
+                ValidationResult.create (value <> notEqualTo) value (messageOrDefault field message defaultMessage)    
 
     type ComparisonValidator<'a when 'a : comparison>() = 
         inherit EqualityValidator<'a>()
 
-        member _.between (min : 'a) (max : 'a) : Validator<'a> =
-            fun (fieldName : string) (message : string option) (value : 'a) -> 
-                let defaultMessage () = sprintf "%s must be between %A and %A" fieldName min max
-                ValidationResult.create (value >= min && value <= max) value (messageOrDefault fieldName message defaultMessage)
+        member _.between (min : 'a) (max : 'a) (message : string option) : Validator<'a> =
+            fun (field : string) (value : 'a) -> 
+                let defaultMessage () = sprintf "%s must be between %A and %A" field min max
+                ValidationResult.create (value >= min && value <= max) value (messageOrDefault field message defaultMessage)
                 
-        member _.greaterThan (min : 'a) : Validator<'a> =
-            fun (fieldName : string) (message : string option) (value : 'a) -> 
-                let defaultMessage () = sprintf "%s must be greater than or equal to %A" fieldName min
-                ValidationResult.create (value > min) value (messageOrDefault fieldName message defaultMessage)
+        member _.greaterThan (min : 'a) (message : string option) : Validator<'a> =
+            fun (field : string) (value : 'a) -> 
+                let defaultMessage () = sprintf "%s must be greater than or equal to %A" field min
+                ValidationResult.create (value > min) value (messageOrDefault field message defaultMessage)
 
-        member _.lessThan (max : 'a) : Validator<'a> =
-            fun (fieldName : string) (message : string option) (value : 'a) -> 
-                let defaultMessage () = sprintf "%s must be less than or equal to %A" fieldName min
-                ValidationResult.create (value < max) value (messageOrDefault fieldName message defaultMessage)
+        member _.lessThan (max : 'a) (message : string option) : Validator<'a> =
+            fun (field : string) (value : 'a) -> 
+                let defaultMessage () = sprintf "%s must be less than or equal to %A" field min
+                ValidationResult.create (value < max) value (messageOrDefault field message defaultMessage)
 
     type StringValidator() =
         inherit EqualityValidator<string>() 
 
-        member _.betweenLen (min : int) (max : int) (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must be between %i and %i characters" fieldName min max
-            ValidationResult.create (value.Length >= min && value.Length <= max) value (messageOrDefault fieldName message defaultMessage)
+        member _.betweenLen (min : int) (max : int) (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must be between %i and %i characters" field min max
+                ValidationResult.create (value.Length >= min && value.Length <= max) value (messageOrDefault field message defaultMessage)
 
-        member _.empty (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must be empty" fieldName
-            ValidationResult.create (strEmpty value) value (messageOrDefault fieldName message defaultMessage)
+        member _.empty (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must be empty" field
+                ValidationResult.create (strEmpty value) value (messageOrDefault field message defaultMessage)
 
-        member _.greaterThanLen (max : int) (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must not execeed %i characters" fieldName max
-            ValidationResult.create (value.Length < max) value (messageOrDefault fieldName message defaultMessage)
+        member _.greaterThanLen (max : int) (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must not execeed %i characters" field max
+                ValidationResult.create (value.Length < max) value (messageOrDefault field message defaultMessage)
 
-        member _.lessThanLen (min : int) (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must be at least %i characters" fieldName min
-            ValidationResult.create (value.Length > min) value (messageOrDefault fieldName message defaultMessage)
+        member _.lessThanLen (min : int) (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must be at least %i characters" field min
+                ValidationResult.create (value.Length > min) value (messageOrDefault field message defaultMessage)
 
-        member _.notEmpty (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must not be empty" fieldName
-            ValidationResult.create (strNotEmpty value) value (messageOrDefault fieldName message defaultMessage)
+        member _.notEmpty (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must not be empty" field
+                ValidationResult.create (strNotEmpty value) value (messageOrDefault field message defaultMessage)
 
-        member _.pattern (pattern : string) (fieldName : string) (message : string option) (value : string) =
-            let defaultMessage () = sprintf "%s must match pattern %s" fieldName pattern
-            ValidationResult.create (Text.RegularExpressions.Regex.IsMatch(value, pattern)) value (messageOrDefault fieldName message defaultMessage)
+        member _.pattern (pattern : string) (message : string option) : Validator<string> =
+            fun (field : string) (value : string) ->
+                let defaultMessage () = sprintf "%s must match pattern %s" field pattern
+                ValidationResult.create (Text.RegularExpressions.Regex.IsMatch(value, pattern)) value (messageOrDefault field message defaultMessage)
 
-    let optional (validator : Validator<'a>) (fieldName : string) (message : string option) (value : 'a option) : ValidationResult<'a option> =  
-        match value with
-        | Some v -> validator fieldName message v |> Result.map (fun v -> Some v)
-        | None   -> Ok value
+    //let optional (validator : Validator<'a>) (field : string) (value : 'a option): ValidationResult<'a option> =  
+    //    match value with
+    //    | Some v -> validator field v |> Result.map (fun v -> Some v)
+    //    | None   -> Success value
 
-    let required (validator : string option -> 'a -> ValidationResult<'a>) (fieldName : string) (message : string option) (value : 'a option) : ValidationResult<'a> =  
-        let defaultMessage () = sprintf "%s is required" fieldName
-        match value with
-        | Some v -> validator message v
-        | None   -> Error (messageOrDefault fieldName message defaultMessage)
+    //let required (validator : string option -> 'a -> ValidationResult<'a>) (fieldName : string) (message : string option) (value : 'a option) : ValidationResult<'a> =  
+    //    let defaultMessage () = sprintf "%s is required" fieldName
+    //    match value with
+    //    | Some v -> validator message v
+    //    | None   -> Error (messageOrDefault fieldName message defaultMessage)
             
     let DateTime       = ComparisonValidator<DateTime>()
     let DateTimeOffset = ComparisonValidator<DateTimeOffset>()
     let Decimal        = ComparisonValidator<decimal>()
     let Float          = ComparisonValidator<float>()
-    let Int            = ComparisonValidator<int>()
+    let Int            = ComparisonValidator<int>()    
     let Int16          = ComparisonValidator<int16>()
     let Int64          = ComparisonValidator<int64>()
     let String         = StringValidator()
     let TimeSpan       = ComparisonValidator<TimeSpan>()
+
+/// Custom operators for ValidationResult
+module Operators =
+    let (<*>) = ValidationResult.apply
+    let (<!>) = ValidationResult.map
+    let (<+>) = Validator.compose
