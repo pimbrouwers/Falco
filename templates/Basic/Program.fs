f1b346713e13d665d1149231a321ae07041e7fe1
Pim Brouwers <pimnation@gmail.com>
2020-11-26 16:23:46 -0500

    basic sample app updated to 3.0.0

--- templates/Basic/Program.fs
+++ templates/Basic/Program.fs
@@ -2,24 +2,10 @@ module AppName.Program
 
 open Falco
 open Falco.Routing
+open Falco.HostBuilder
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
-
-// ------------
-// Handlers 
-// ------------
-let handlePlainText : HttpHandler =
-    Response.ofPlainText "Hello from /"
-
-// ------------
-// Routes
-// ------------
-let endpoints =
-    [            
-        get "/" handlePlainText
-    ]
 
 // ------------
 // Register services
@@ -30,7 +16,7 @@ let configureServices (services : IServiceCollection) =
 // ------------
 // Activate middleware
 // ------------
-let configureApp (ctx : WebHostBuilderContext) (app : IApplicationBuilder) =    
+let configureApp (endpoints : HttpEndpoint list) (ctx : WebHostBuilderContext) (app : IApplicationBuilder) =    
     let devMode = StringUtils.strEquals ctx.HostingEnvironment.EnvironmentName "Development"    
     app.UseWhen(devMode, fun app -> 
             app.UseDeveloperExceptionPage())
@@ -38,19 +24,20 @@ let configureApp (ctx : WebHostBuilderContext) (app : IApplicationBuilder) =
             app.UseFalcoExceptionHandler(Response.withStatusCode 500 >> Response.ofPlainText "Server error"))
        .UseFalco(endpoints) |> ignore
 
+// -----------
+// Configure Web host
+// -----------
+let configureWebHost (endpoints : HttpEndpoint list) (webHost : IWebHostBuilder) =
+    webHost
+        .ConfigureServices(configureServices)
+        .Configure(configureApp endpoints)
+
 [<EntryPoint>]
-let main args =    
-    try
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webhost ->   
-                webhost
-                    .ConfigureServices(configureServices)
-                    .Configure(configureApp)
-                    |> ignore)
-            .Build()
-            .Run()                        
-        0
-    with 
-    | ex -> 
-        printfn "%s\n\n%s" ex.Message ex.StackTrace
-        -1
+let main args =   
+    webHost args {
+        configure configureWebHost
+        endpoints [            
+            get "/" (Response.ofPlainText "Hello world")
+        ]
+    }
+    0
