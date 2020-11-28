b251023d8f2965297774bf64300e355e32c6e6b2
Jon Canning <jon.canning@gmail.com>
2020-11-28 21:58:49 +0000

    Response.ofHtmlString

--- src/Falco.Tests/ResponseTests.fs
+++ src/Falco.Tests/ResponseTests.fs
@@ -17,35 +17,35 @@ let ``Response.withStatusCode should modify HttpResponse StatusCode`` () =
     let expected = 204
 
     task {
-        do! ctx 
+        do! ctx
             |> (Response.withStatusCode expected >> Response.ofEmpty)
-        
-        ctx.Response.StatusCode 
+
+        ctx.Response.StatusCode
         |> should equal expected
     }
 
 [<Fact>]
 let ``Response.withHeader should set header`` () =
     let serverName = "Kestrel"
-    let ctx = getHttpContextWriteable false    
-    
+    let ctx = getHttpContextWriteable false
+
     task {
-        do! ctx 
+        do! ctx
             |> (Response.withHeader HeaderNames.Server serverName >> Response.ofEmpty)
-        
+
         ctx.Response.Headers.[HeaderNames.Server]
         |> should equal serverName
     }
 
 [<Fact>]
 let ``Response.withContentType should set header`` () =
-    let contentType = "text/plain; charset=utf-8" 
-    let ctx = getHttpContextWriteable false    
-    
+    let contentType = "text/plain; charset=utf-8"
+    let ctx = getHttpContextWriteable false
+
     task {
-        do! ctx 
+        do! ctx
             |> (Response.withHeader HeaderNames.ContentType contentType>> Response.ofEmpty)
-        
+
         ctx.Response.Headers.[HeaderNames.ContentType]
         |> should equal contentType
     }
@@ -59,7 +59,7 @@ let ``Response.redirect temporary should invoke HttpResponse Redirect with provi
     task {
         do! ctx
             |> Response.redirect "/" permanent
-                    
+
         ctx.Response.Received().Redirect("/", permanent)
     }
 
@@ -72,9 +72,9 @@ let ``Response.ofPlainText produces text/plain result`` () =
     task {
         do! ctx
             |> Response.ofPlainText expected
-        
+
         let! body = getResponseBody ctx
-        let contentLength = ctx.Response.ContentLength        
+        let contentLength = ctx.Response.ContentLength
         let contentType = ctx.Response.Headers.[HeaderNames.ContentType]
 
         body          |> should equal expected
@@ -93,7 +93,7 @@ let ``Response.ofJson produces applicaiton/json result`` () =
             |> Response.ofJson { Name = "John Doe"}
 
         let! body = getResponseBody ctx
-        let contentLength = ctx.Response.ContentLength        
+        let contentLength = ctx.Response.ContentLength
         let contentType = ctx.Response.Headers.[HeaderNames.ContentType]
 
         body          |> should equal expected
@@ -115,7 +115,7 @@ let ``Response.ofJsonOptions produces applicaiton/json result ignoring nulls`` (
             |> Response.ofJsonOptions jsonOptions { Name = null }
 
         let! body = getResponseBody ctx
-        let contentLength = ctx.Response.ContentLength        
+        let contentLength = ctx.Response.ContentLength
         let contentType = ctx.Response.Headers.[HeaderNames.ContentType]
 
         body          |> should equal expected
@@ -129,7 +129,7 @@ let ``Response.ofHtml produces text/html result`` () =
 
     let expected = "<!DOCTYPE html><html><div class=\"my-class\"><h1>hello</h1></div></html>"
 
-    let doc = 
+    let doc =
         Elem.html [] [
                 Elem.div [ Attr.class' "my-class" ] [
                         Elem.h1 [] [ Text.raw "hello" ]
@@ -141,7 +141,26 @@ let ``Response.ofHtml produces text/html result`` () =
             |> Response.ofHtml doc
 
         let! body = getResponseBody ctx
-        let contentLength = ctx.Response.ContentLength        
+        let contentLength = ctx.Response.ContentLength
+        let contentType = ctx.Response.Headers.[HeaderNames.ContentType]
+
+        body          |> should equal expected
+        contentLength |> should equal (Encoding.UTF8.GetBytes expected).LongLength
+        contentType   |> should equal "text/html; charset=utf-8"
+    }
+
+[<Fact>]
+let ``Response.ofHtmlString produces text/html result`` () =
+    let ctx = getHttpContextWriteable false
+
+    let expected = "<!DOCTYPE html><html><div class=\"my-class\"><h1>hello</h1></div></html>"
+
+    task {
+        do! ctx
+            |> Response.ofHtmlString expected
+
+        let! body = getResponseBody ctx
+        let contentLength = ctx.Response.ContentLength
         let contentType = ctx.Response.Headers.[HeaderNames.ContentType]
 
         body          |> should equal expected
