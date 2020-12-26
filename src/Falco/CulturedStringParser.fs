d5ae89f6e94dd40e8d526bead016d9c974915efd
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2020-12-26 20:19:51 +0200

    parseInt aliases parseInt32

--- src/Falco/CulturedStringParser.fs
+++ src/Falco/CulturedStringParser.fs
@@ -3,7 +3,9 @@ module Falco.CulturedStringParser
 open System
 open System.Globalization
 
-let parseInt (culture: string) (readString: string) = 
+let parseInt32 (culture: string) (readString: string) = 
     match Int32.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
     | true, value -> Some value
     | false, _ -> None
+
+let parseInt = parseInt32
