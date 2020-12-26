d69af947d192cbe9c079a32f5f3e1459251f05a2
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2020-12-26 20:26:35 +0200

    Parse int16

--- src/Falco.Tests/CulturedStringParserTests.fs
+++ src/Falco.Tests/CulturedStringParserTests.fs
@@ -7,6 +7,24 @@ open FsUnit.Xunit
 module CulturedStringParser = 
     open Falco.CulturedStringParser
 
+    [<Theory>]
+    [<InlineData("1", 1s)>]
+    [<InlineData("32767", Int16.MaxValue)>]
+    [<InlineData("-32768", Int16.MinValue)>]
+    let ``parseInt16 should be some`` toParse result =
+        toParse
+        |> parseInt16 ""
+        |> should equal (Some result)
+
+    [<Theory>]
+    [<InlineData("")>]
+    [<InlineData("32768")>]
+    [<InlineData("-32769")>]
+    let ``parseInt16 should be none`` toParse =
+        toParse
+        |> parseInt16 ""
+        |> should equal None
+
     [<Theory>]    
     [<InlineData("1", 1)>]    
     [<InlineData("2147483647", 2147483647)>]
--- src/Falco/CulturedStringParser.fs
+++ src/Falco/CulturedStringParser.fs
@@ -3,6 +3,11 @@ module Falco.CulturedStringParser
 open System
 open System.Globalization
 
+let parseInt16 (culture: string) (readString: string) = 
+    match Int16.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
+    | true, value -> Some value
+    | false, _ -> None
+
 let parseInt32 (culture: string) (readString: string) = 
     match Int32.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
     | true, value -> Some value
