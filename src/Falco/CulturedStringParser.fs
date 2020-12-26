b23621c97ec54a57849f629ec64bb3776365aa7c
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2020-12-26 20:36:17 +0200

    Extract tryParse

--- src/Falco/CulturedStringParser.fs
+++ src/Falco/CulturedStringParser.fs
@@ -3,10 +3,13 @@ module Falco.CulturedStringParser
 open System
 open System.Globalization
 
-let parseInt16 (culture: string) (readString: string) = 
-    match Int16.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
+let tryParse parser (culture: string) (readString: string) =
+    match parser (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
     | true, value -> Some value
     | false, _ -> None
+
+let parseInt16 = 
+    tryParse (fun (readString, numberStyle, cultureInfo) -> Int16.TryParse (readString, numberStyle, cultureInfo))
 
 let parseInt32 (culture: string) (readString: string) = 
     match Int32.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
