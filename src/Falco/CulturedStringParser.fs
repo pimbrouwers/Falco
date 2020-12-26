e58a19f088cfe7b2f758784d0e1c53bc8ffb2caf
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2020-12-26 20:08:01 +0200

    Rename s to readString

--- src/Falco/CulturedStringParser.fs
+++ src/Falco/CulturedStringParser.fs
@@ -3,8 +3,7 @@ module Falco.CulturedStringParser
 open System
 open System.Globalization
 
-let parseInt (culture: string) (s: string) = 
-    match Int32.TryParse (s, NumberStyles.Integer, CultureInfo (culture, true)) with
+let parseInt (culture: string) (readString: string) = 
+    match Int32.TryParse (readString, NumberStyles.Integer, CultureInfo (culture, true)) with
     | true, value -> Some value
     | false, _ -> None
-    
