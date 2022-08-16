3cddb16d3998b94772a87fd93563779b39115efb
Pim Brouwers <pimnation@gmail.com>
2022-08-16 16:25:53 -0400

    dedicated svg namespace and elem + attr modules

--- src/Falco.Markup/Templates.fs
+++ src/Falco.Markup/Templates.fs
@@ -16,7 +16,7 @@ module Templates =
 
     /// SVG Version 1.0 template with customizable viewBox width/height
     let svg (x : int, y : int, w : int, h : int) (content : XmlNode list) =
-        Elem.svg [            
-            Attr.create "viewBox" (sprintf "%i %i %i %i" x y w h)
-            Attr.create "xmlns" "http://www.w3.org/2000/svg"
+        Elem.svg [
+            Svg.Attr.viewBox (sprintf "%i %i %i %i" x y w h)
+            Svg.Attr.xmlns "http://www.w3.org/2000/svg"
         ] content
