be77b175091d71bfa9103a40541b98108ba8e99c
Pim Brouwers <pbrouwers@nhlpa.com>
2022-08-22 16:02:07 -0400

    tutorial stub

---
+++ samples/Tutorial/Program.fs
@@ -0,0 +1,24 @@
+module FalcoTutorial.Program
+
+open Falco
+open Falco.Markup
+open Falco.Routing
+open Falco.HostBuilder
+open Microsoft.AspNetCore.Builder
+
+let exceptionHandler : HttpHandler =
+    Response.withStatusCode 500 >> Response.ofPlainText "Server error"
+
+[<EntryPoint>]
+let main args =
+    webHost args {
+        use_ifnot FalcoExtensions.IsDevelopment HstsBuilderExtensions.UseHsts
+        use_https
+        use_static_files
+
+        use_if    FalcoExtensions.IsDevelopment DeveloperExceptionPageExtensions.UseDeveloperExceptionPage
+        use_ifnot FalcoExtensions.IsDevelopment (FalcoExtensions.UseFalcoExceptionHandler exceptionHandler)
+
+        endpoints []
+    }
+    0
