3ecce73c97285b77efe50042e8c67b94377880a9
Pim Brouwers <pbrouwers@nhlpa.com>
2022-08-30 21:20:48 -0400

    routing doc

--- samples/HelloWorld/Program.fs
+++ samples/HelloWorld/Program.fs
@@ -1,50 +1,35 @@
 module HelloWorld.Program
 
-// open Falco
-// open Falco.Markup
-// open Falco.Routing
-// open Falco.HostBuilder
-
-// /// ANY /
-// let handlePlainText : HttpHandler =
-//     // Response.ofPlainText "Hello world"
-//     fun ctx -> failwith "EXCEPTION"
-
-// /// GET /json
-// let handleJson : HttpHandler =
-//     let message = {| Message = "Hello from /json" |}
-//     Response.ofJson message
-
-// /// GET /html
-// let handleHtml : HttpHandler =
-//     let html =
-//         Templates.html5 "en"
-//             [ Elem.link [ Attr.href "style.css"; Attr.rel "stylesheet" ] ]
-//             [ Elem.h1 [] [ Text.raw "Hello from /html" ] ]
-
-//     Response.ofHtml html
-
-// webHost [||] {
-//     use_https
-
-//     endpoints [
-//         get "/html" handleHtml
-
-//         get "/json" handleJson
-
-//         any "/" handlePlainText
-//     ]
-// }
 open Falco
+open Falco.Markup
 open Falco.Routing
 open Falco.HostBuilder
 
+/// ANY /
+let handlePlainText : HttpHandler =
+    // Response.ofPlainText "Hello world"
+    fun ctx -> failwith "EXCEPTION"
+
+/// GET /json
+let handleJson : HttpHandler =
+    let message = {| Message = "Hello from /json" |}
+    Response.ofJson message
+
+/// GET /html
+let handleHtml : HttpHandler =
+    let html =
+        Templates.html5 "en"
+            [ Elem.link [ Attr.href "style.css"; Attr.rel "stylesheet" ] ]
+            [ Elem.h1 [] [ Text.raw "Hello from /html" ] ]
+
+    Response.ofHtml html
+
 webHost [||] {
+    use_https
+
     endpoints [
-        any "/"      (Response.ofPlainText "/")
-        get "/hello" (Response.ofPlainText "/hello")
-        get "/debug" Request.debug
-        all "/form"  [GET, Response.ofPlainText "/form"
-                      POST, Request.mapJson Response.ofJson]
+        get "/json" handleJson
+
+        any "/" handlePlainText
     ]
 }
