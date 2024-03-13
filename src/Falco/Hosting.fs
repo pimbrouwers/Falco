756f3e9a74a76d2f527a222b072c948d17de3856
Pim Brouwers <pbrouwers@nhlpa.com>
2024-03-13 08:09:13 -0400

    hosting

--- src/Falco/Hosting.fs
+++ src/Falco/Hosting.fs
@@ -3,25 +3,23 @@ namespace Falco
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Routing
 
-/// Represents a missing dependency, thrown on request.
-exception InvalidDependencyException of string
-
 [<AutoOpen>]
 module Extensions =
     type IEndpointRouteBuilder with
         /// Activates Falco Endpoint integration.
-        member x.UseFalcoEndpoints (endpoints : HttpEndpoint seq) =
+        member x.UseFalcoEndpoints(endpoints : HttpEndpoint seq) =            
             let dataSource = FalcoEndpointDatasource(endpoints)
             x.DataSources.Add(dataSource)
 
     type IApplicationBuilder with
         /// Activates Falco integration with IEndpointRouteBuilder.
-        member x.UseFalco (endpoints : HttpEndpoint seq) =
-            x.UseEndpoints(fun r -> r.UseFalcoEndpoints(endpoints))
+        member x.UseFalco(endpoints : HttpEndpoint seq) =
+            x.UseRouting()
+             .UseEndpoints(fun r -> r.UseFalcoEndpoints(endpoints))
 
         /// Registers a Falco HttpHandler as exception handler lambda.
         /// See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/error-handling?#exception-handler-lambda
-        member x.UseFalcoExceptionHandler (exceptionHandler : HttpHandler) =
+        member x.UseFalcoExceptionHandler(exceptionHandler : HttpHandler) =
             let configure (appBuilder : IApplicationBuilder) =
                 appBuilder.Run(HttpHandler.toRequestDelegate exceptionHandler)
 
