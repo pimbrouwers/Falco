3e5a9567bbb906ae8f98a5ee7a38ac4ae665fce0
Pim Brouwers <pimnation@gmail.com>
2024-03-30 17:05:13 -0400

    dependency injection example renamed

--- examples/DependencyInjection/DependencyInjection.fs
+++ examples/DependencyInjection/DependencyInjection.fs
@@ -20,9 +20,9 @@ let wapp = bldr.Build()
 
 wapp.UseFalco()
     .FalcoGet("/{name?}", fun ctx ->
-        let greeter = ctx.Plug<IGreeter>()
+        let greeter = ctx.Plug<IGreeter>() // <-- access our dependency from the container
         let route = Request.getRoute ctx
         let name = route.GetString("name", "world")
-        let greeting = greeter.Greet(name)
+        let greeting = greeter.Greet(name) // <-- invoke our greeter.Greet(name) method
         Response.ofPlainText greeting ctx)
     .Run()
