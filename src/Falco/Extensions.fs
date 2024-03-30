2e1726bbaf64d1de5ac845dcf863b4407240fa66
Pim Brouwers <pimnation@gmail.com>
2024-03-30 14:18:26 -0400

    falco extension comments

--- src/Falco/Extensions.fs
+++ src/Falco/Extensions.fs
@@ -14,6 +14,7 @@ module Extensions =
             x.RequestServices.GetRequiredService<'T>()
 
     type WebApplicationBuilder with
+        /// Apply `fn` to `WebApplicationBuilder.Services :> IServiceCollection`  if `predicate` is true.
         member x.AddIf(predicate : bool, fn : IConfiguration -> IServiceCollection -> IServiceCollection) : WebApplicationBuilder =
             if predicate then fn x.Configuration x.Services |> ignore
             x
@@ -28,7 +29,7 @@ module Extensions =
         member x.Use(fn : IApplicationBuilder -> IApplicationBuilder) : WebApplication =
             x.UseIf(true, fn)
 
-        /// Registers a Falco HttpHandler as exception handler lambda.
+        /// Registers a `Falco.HttpHandler` as exception handler lambda.
         /// See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/error-handling?#exception-handler-lambda
         member x.UseFalcoExceptionHandler(exceptionHandler : HttpHandler) : WebApplication =
             let configure (appBuilder : IApplicationBuilder) =
@@ -45,6 +46,7 @@ module Extensions =
                 endpointBuilder.DataSources.Add(dataSource)) |> ignore
             x
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder`.
         member x.MapFalco(endpoint : HttpEndpoint) : WebApplication =
             for (verb, handler) in endpoint.Handlers do
                 match verb with
@@ -60,42 +62,55 @@ module Extensions =
                 |> ignore
             x
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches HTTP GET requests for the specified pattern.
         member x.FalcoGet(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ GET, handler ] })
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches HTTP POST requests for the specified pattern.
         member x.FalcoPost(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ POST, handler ] })
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches HTTP PUT requests for the specified pattern.
         member x.FalcoPut(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ PUT, handler ] })
-
+        
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches HTTP DELETE requests for the specified pattern.
         member x.FalcoDelete(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ DELETE, handler ] })
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches HTTP PATCH requests for the specified pattern.
         member x.FalcoPatch(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ PATCH, handler ] })
 
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches all HTTP requests for the specified pattern.
         member x.FalcoAny(pattern : string, handler : HttpHandler) : WebApplication =
             x.MapFalco({
                 Pattern = pattern
                 Handlers = [ ANY, handler ] })
+
+        /// Adds a `Falco.HttpEndpoint` to the `Microsoft.AspNetCore.Routing.IEndpointRouteBuilder` that matches the provided HTTP requests for the specified pattern.
+        member x.FalcoAll(pattern : string, handlers : (HttpVerb * HttpHandler) seq) : WebApplication =
+            x.MapFalco({
+                Pattern = pattern
+                Handlers = handlers })
         
-        /// Registers a Falco HttpHandler as terminal middleware (i.e., not found).
+        /// Registers a `Falco.HttpHandler` as terminal middleware (i.e., not found).
         member x.FalcoNotFound(handler : HttpHandler) : WebApplication =
             x.Run(handler = HttpHandler.toRequestDelegate handler) |> ignore
             x
 
     type FalcoExtensions =
+        /// Registers a `Falco.HttpHandler` as global exception handler.
         static member UseFalcoExceptionHandler
             (exceptionHandler : HttpHandler)
             (app : WebApplication) =
