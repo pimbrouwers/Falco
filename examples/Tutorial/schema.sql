2a1e968e78d92d7b4cd60c173f7c77c9b43e4814
Pim Brouwers <pimnation@gmail.com>
2024-08-31 07:42:35 -0400

    tutorial removed

--- examples/Tutorial/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS entry;
-
-CREATE TABLE entry (
-	  entry_id       TEXT     NOT NULL  PRIMARY KEY
-  , html_content   TEXT     NOT NULL
-  , text_content   TEXT     NOT NULL
-  , modified_date  TEXT     NOT NULL);
