a282162c536e78098cf539fe9bbf044cc3b55ebf
Pim Brouwers <pimnation@gmail.com>
2024-11-18 09:21:10 -0500

    net9.0 support

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param (
     [Parameter(HelpMessage="The action to execute.")]
-    [ValidateSet("Build", "Test", "Pack", "BuildSite", "DevelopSite")]
+    [ValidateSet("Build", "Test", "IntegrationTest", "Pack", "BuildSite", "DevelopSite")]
     [string] $Action = "Build",
 
     [Parameter(HelpMessage="The msbuild configuration to use.")]
@@ -9,7 +9,7 @@ param (
     [string] $Configuration = "Debug",
 
     [switch] $NoRestore,
-    
+
     [switch] $Clean
 )
 
@@ -24,26 +24,27 @@ $srcDir = Join-Path -Path $rootDir -ChildPath 'src'
 $testDir = Join-Path -Path $rootDir -ChildPath 'test'
 
 switch ($Action) {
-    "Test"        { $projectdir = Join-Path -Path $testDir -ChildPath 'Falco.Tests' }
-    "Pack"        { $projectDir = Join-Path -Path $srcDir -ChildPath 'Falco' }
-    "BuildSite"   { $projectDir = Join-Path -Path $rootDir -ChildPath 'site' }
-    "DevelopSite" { $projectDir = Join-Path -Path $rootDir -ChildPath 'site' }
-    Default       { $projectDir = Join-Path -Path $srcDir -ChildPath 'Falco' }
+    "Test"            { $projectdir = Join-Path -Path $testDir -ChildPath 'Falco.Tests' }
+    "IntegrationTest" { $projectdir = Join-Path -Path $testDir -ChildPath 'Falco.IntegrationTests' }
+    "Pack"            { $projectDir = Join-Path -Path $srcDir -ChildPath 'Falco' }
+    "BuildSite"       { $projectDir = Join-Path -Path $rootDir -ChildPath 'site' }
+    "DevelopSite"     { $projectDir = Join-Path -Path $rootDir -ChildPath 'site' }
+    Default           { $projectDir = Join-Path -Path $srcDir -ChildPath 'Falco' }
 }
 
 if(!$NoRestore.IsPresent) {
     RunCommand "dotnet restore $projectDir --force --force-evaluate --nologo --verbosity quiet"
 }
 
-if ($Clean)
-{
+if ($Clean) {
     RunCommand "dotnet clean $projectDir -c $Configuration --nologo --verbosity quiet"
 }
 
 switch ($Action) {
-    "Test"        { RunCommand "dotnet test `"$projectDir`"" }
-    "Pack"        { RunCommand "dotnet pack `"$projectDir`" -c $Configuration --include-symbols --include-source" }
-    "BuildSite"   { RunCommand "dotnet build `"$projectDir`" -t:Generate" }
-    "DevelopSite" { RunCommand "dotnet build `"$projectDir`" -t:Develop" }
-    Default       { RunCommand "dotnet build `"$projectDir`" -c $Configuration" }
+    "Test"            { RunCommand "dotnet test `"$projectDir`"" }
+    "IntegrationTest" { RunCommand "dotnet test `"$projectDir`"" }
+    "Pack"            { RunCommand "dotnet pack `"$projectDir`" -c $Configuration --include-symbols --include-source" }
+    "BuildSite"       { RunCommand "dotnet build `"$projectDir`" -t:Generate" }
+    "DevelopSite"     { RunCommand "dotnet build `"$projectDir`" -t:Develop" }
+    Default           { RunCommand "dotnet build `"$projectDir`" -c $Configuration" }
 }
